eb0748876fb4590ddda619576d82c5bd53630793
slkennedy <slkennedy@gmail.com>
2014-10-07 00:35:58 -0400

    Another commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,79 @@
-console.log('The Iron Yard Rocks');
+(function(){
+	'use strict';
+
+var Items = Backbone.Model.extend ({});
+
+var ItemsCollection = Backbone.Collection.extend ({
+	model: Items
+});
+	//Overall List View
+	var CreateListView = Backbone.View.extend ({
+		tagName: 'ul',
+		className: 'the-list',
+
+		initialize: function (){
+			$('body').append(this.el);
+
+			this.listenTo(this.collection, 'add', function(item){
+				this.$el.append('<li>' +item.get('name')+ '</li>' + '<button class="delete">' + 'Delete' + '</button>' + '<button class = "done">' +'Done'+ '</button>');
+   			});
+		},
+
+		events: {
+			'remove':'deleteItem'
+		},
+
+		deleteItem: function (){
+		 	.remove('')
+		}
+
+	});
+
+	//Items View
+	var CreateItemView = Backbone.View.extend ({ 
+		tagName: 'li',
+		className: 'the-items',
+
+		initialize: function (){
+			$('.the-list').append(this.el);
+		}
+	});
+
+	//Input View
+	var CreateInputView = Backbone.View.extend ({
+		tagName: 'input',
+		className: 'the-input',
+		attributes: {
+			type: 'text'
+		},
+
+		events: {
+			'keyup': 'addItem'
+		},
+
+		addItem: function(event){
+    		if(event.keyCode === 13){
+				var item = this.collection.add({name: this.$el.val()})
+			}
+		},
+
+		render: function(){
+			$('body').prepend(this.el);
+		}
+	});
+
+	//Glue Code
+	$(document).ready(function (){ 
+		var collectionItem = new ItemsCollection;
+		
+		var inputView = new CreateInputView({collection: collectionItem});
+		inputView.render();	
+
+		var listView = new CreateListView({collection: collectionItem});
+		listView.render ();
+
+		var itemView = new CreateItemView({collection: collectionItem});
+		itemView.render();
+	});
+
+})();
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,10 +3,28 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('List Tests', function () {
+        describe('Is there a list', function () {
+            it('should have a list', function () {
+            	var list = [];
+            	expect(list).to.be.ok;
+            });
+            it('should have more than one item on the list', function (){
+           		var list = [{name: 'Finish Project'}, {name: 'Eat Dinner'}];
 
+            	expect($(list).length).to.be.above(1);
+            })
+            it('should have properties of name and done', function (){
+            	var list = [{name: 'Finish Project', done: false}, {name: 'Eat Dinner', done:false}];
+
+            	expect(list[0]).to.contain.keys('name', 'done');
+            })
+            it('should be inserted into the DOM', function () {
+            	$(document).ready(function () {
+            		var list = [{name: 'Finish Project', done: false}, {name: 'Eat Dinner', done:false}];
+            	})
+
+            	expect($('list')).to.be.ok;
             });
         });
     });
